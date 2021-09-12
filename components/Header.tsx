8f12f25ad9f9827ce7fe8daf5d001aa2cb837d47
saurabh <saurabhmehta1601@gmail.com>
2021-09-12 14:14:43 +0530

    chore: initial commit
    setup create-next-app with typescript template and tailwindcss

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <span className="text-2xl">ChatApp</span>
+        </div>
+    )
+}
+
+<span></span>
+export default Header
