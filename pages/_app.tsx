a8e5ebf00bfc084e215c736fa1690ff3d77fef6b
saurabh <saurabhmehta1601@gmail.com>
2021-09-12 17:31:27 +0530

    feat(pages/_app.ts): setup graphql with @apollo/client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import { ApolloProvider } from '@apollo/client'
+import client from "../apollo-client"
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<ApolloProvider client={client}>
+      <Component {...pageProps} />
+  </ApolloProvider>)
 }
 export default MyApp
